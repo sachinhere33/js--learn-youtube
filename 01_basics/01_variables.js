b32f40197da98a2c1ae5bfa032117d8d14d8dcc5
sachinhere33 <162969069+sachinhere33@users.noreply.github.com>
2024-04-27 18:49:52 +0000

    variable discuss

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+
+let accountEmail = sarthak12@gmail.com
+var accountPassword = "12345"
+accountCity = "noida"
+
+
+// accountId = 2 // not alowed
+
+accountEmail = sv@gmail.com
+accontPassword = "242424"
+accountCity = "pune"
+console.log(accountId):
+console.table([accountId,accountEmail,accountPassword,accountCity])

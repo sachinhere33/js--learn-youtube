09f2dc5d3104c2a22beb9a668ced460db9bfac13
sachinhere33 <162969069+sachinhere33@users.noreply.github.com>
2024-05-02 07:52:02 +0000

    string to number

--- 01_basics/03_consionOperation.js
+++ 01_basics/03_consionOperation.js
@@ -1,5 +1,27 @@
 let score = "sachin"
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// ********* operations *********
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 ='hello'
+let str2 = " sachin"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1"+2);
+console.log(1+2+"2";)
+console.log(1+"2");
+

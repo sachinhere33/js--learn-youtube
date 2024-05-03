21727e7d8c0ec0973cca04976986cdf511e07eb8
sachinhere33 <gotitsachin300@gmail.com>
2024-05-03 17:52:37 +0000

    array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+// arrays 
+const myArr = [0,1,2,3,4]
+const myHeros = ["abhinav","awadh"]
+
+const myArr2 = ne Array(1,2,3,4)
+// rray methods 
+
+myArr.push(6)
+console.log(myArr);
+myArr.pop()
+myArr.unshift(9)
+console.log(myArr);
+
+// slice and splice
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+//                part2
+
+
+
+
+

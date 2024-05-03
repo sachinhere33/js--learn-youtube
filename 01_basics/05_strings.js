e4daddded300aaa4a089054ff9d51cc5cb6f0884
sachinhere33 <gotitsachin300@gmail.com>
2024-05-03 15:25:01 +0000

    strings discuss

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "sarthak"
+const repoCount = 50
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String(`sachin`)
+console.log(gameName[0]);
+
+console.log(gameName.indexOf(t));
+const newString = gameName.substring(0,4)
+console.log(newString)
+const url = "http://sachin.com/sarthak%20vats"
+console.log(url.replace('%20','-'))

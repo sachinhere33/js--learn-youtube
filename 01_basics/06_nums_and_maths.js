7f5b37941c0d79eff0918e5fa15eebd11344e22b
sachinhere33 <gotitsachin300@gmail.com>
2024-05-03 16:41:20 +0000

    numbers and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundreds =100000
+console.log(hundreds.toLocaleString(`en-IN`));
+
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// onslotchange.log(Math.ceil(4.2));
+console.log(Math.random());
+console.log(Math.random()*10) + 1);
+
+
+
+
+
+
+
+

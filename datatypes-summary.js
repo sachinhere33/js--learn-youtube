405fc7800e852b270d8dbff7797c78198eabddad
sachinhere33 <gotitsachin300@gmail.com>
2024-05-03 14:44:02 +0000

    stack and heap memory

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -1,14 +1,27 @@
-const score = 100
-const loggeIn = false
-const id = Symbol('123')
-const anotherId = Symbol('123')
-const bigNuber = 3456656n
+// const score = 100
+// const loggeIn = false
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// const bigNuber = 3456656n
 
-const heros = ["marvel","shaktiman","naruto"]
-let myObj = {
-    name:"sachin"
-    age: 24
+// const heros = ["marvel","shaktiman","naruto"]
+// let myObj = {
+//     name:"sachin"
+//     age: 24
+// }
+// const myFunction = function(){
+//     console.log("hello world");
 }
-const myFunction = function(){
-    console.log("hello world");
+let anothername = myYoutubename
+anothername = "codewithsachin"
+ console.log(myYoutubename)
+ console.log(anothername)
+  let userOne = {
+    email: "user@gmail.com"
+    upi: "user@ybl"
+  
 }
+let userTwo = userOne
+  userTwo.email = "sarthak@google.com"
+   console.log(userOne.email);
+   console.log(userTwo.email);

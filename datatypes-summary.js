5f92dac7e4bb94f85dfc7435bbc08db70ff8999e
sachinhere33 <gotitsachin300@gmail.com>
2024-05-03 13:53:00 +0000

    all datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,14 @@
+const score = 100
+const loggeIn = false
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const bigNuber = 3456656n
+
+const heros = ["marvel","shaktiman","naruto"]
+let myObj = {
+    name:"sachin"
+    age: 24
+}
+const myFunction = function(){
+    console.log("hello world");
+}
